6d53162bc6abf72da17275ff7cb4f400f3a60f47
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-05-16 16:56:50 +0700

    Fix Indent

--- src/without-input/maximum_number.nim
+++ src/without-input/maximum_number.nim
@@ -1,7 +1,7 @@
 let x = [1, 4, 2, 3, 5, 8, 9, 7, 6, 0]
 var max = x[0]
 for i in 1..high(x):
-    if max < x[i]:
-        max = x[i]
-        
+  if max < x[i]:
+    max = x[i]
+
 echo "Nilai maksimal : ", max
